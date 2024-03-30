9d870a850938aa780ccd9b78819a28c201d72800
Esraa102 <manalellban9@gmailcom>
2024-03-30 20:10:41 +0200

    create api test routes

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { connectToDB } from "./config/connectDB.js";
+import { userRouter } from "./routes/user.route.js";
 
 const app = express();
-
 connectToDB();
+
+app.use(express.json());
+app.use("/api/users", userRouter);
 app.listen(5000, () => {
   console.log("Server Is Running On 5000");
 });
