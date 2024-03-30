261d9fa0888d81035ab402a1b8dcda651c597894
Esraa102 <manalellban9@gmailcom>
2024-03-30 19:46:16 +0200

    connect to mogodb database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectToDB } from "./config/connectDB.js";
+
 const app = express();
 
+connectToDB();
 app.listen(5000, () => {
   console.log("Server Is Running On 5000");
 });
+//EpUPYROlsVtOrHmk
