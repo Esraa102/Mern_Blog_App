6ba614ba9b1d01106f341c0c14a88759befdcb1f
Esraa102 <manalellban9@gmailcom>
2024-03-31 03:28:06 +0200

    add redux persist to keep data

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./redux/stor.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/stor.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate persistor={persistor} loading={null}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
--- frontend/src/redux/stor.js
+++ frontend/src/redux/stor.js
@@ -1,9 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/userSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userSlice });
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persietedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user: userSlice },
+  reducer: persietedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
