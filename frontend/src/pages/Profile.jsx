4088e8df8f7537155cc7fcac5e35a2b649771463
Esraa102 <manalellban9@gmailcom>
2024-03-31 23:38:50 +0200

    compelet upload image

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,24 +1,67 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import toast from "react-hot-toast";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
 const Profile = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [imgData, setImgData] = useState(currentUser.imgProfile);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [formData, setFormData] = useState({
+    imgProfile: currentUser.imgProfile,
+  });
+  const [uploadSucess, setUploadSucess] = useState(false);
+  const handleFileUpload = async (img) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + img.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, img);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgress(Math.round(progress));
+      },
+      (error) => {
+        console.log(error);
+        toast.error("File size must be less than 2MB");
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, imgProfile: downloadURL });
+          setUploadSucess(true);
+          toast.success("Image Uploaded Successfully");
+        });
+      }
+    );
+  };
+  useEffect(() => {
+    if (imgData) {
+      handleFileUpload(imgData);
+    }
+  }, [imgData]);
   return (
     <section>
       <div className="section-container">
         <div className="relative w-fit mx-auto">
           <img
-            src={imgData}
+            src={formData.imgProfile}
             alt="profile-img"
             className="w-[200px] h-[200px] object-cover mx-auto mt-6 rounded-full"
           />
           <div className="absolute bottom-2 right-4">
             <input
               type="file"
-              accept="png,jpg,jpeg"
-              onChange={(e) =>
-                setImgData(URL.createObjectURL(e.target.files[0]))
-              }
+              accept="image/*"
+              onChange={(e) => setImgData(e.target.files[0])}
               name="img"
               id="img"
               className="hidden"
@@ -35,6 +78,11 @@ const Profile = () => {
             </label>
           </div>
         </div>
+        {uploadProgress > 0 && !uploadSucess && (
+          <p className="text-center my-3 text-yellow-500 font-semibold">
+            Uploading {uploadProgress}%
+          </p>
+        )}
         <h1 className="text-3xl sm:text-4xl font-bold text-center my-6 capitalize text-main">
           {currentUser.username}
         </h1>
@@ -50,8 +98,9 @@ const Profile = () => {
             <input
               type="email"
               name="email"
+              readOnly
               defaultValue={currentUser.email}
-              className="px-3 py-2 bg-[#1E293B] rounded-md focus:outline-none text-white focus:border focus:border-main"
+              className="px-3 py-2 bg-[#1E293B] rounded-md focus:outline-none opacity-50"
               placeholder="Your Email"
             />
             <input
