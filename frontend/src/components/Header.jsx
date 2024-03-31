b1d2f1e03703a0b97a95017483a9464a6b5709b8
Esraa102 <manalellban9@gmailcom>
2024-03-31 22:23:07 +0200

    update header ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import LogOut from "./LogOut";
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <header className="text-white fixed backdrop-blur-md top-0 left-0 w-full">
+    <header className="text-white z-10 fixed backdrop-blur-md top-0 left-0 w-full">
       <div className="container mx-auto py-6 px-4 flex gap-4 items-center justify-between">
         <Link to={"/"} className="text-2xl font-bold">
           BLOG
