fdbe238585d2d9d4ae4d648de7e8c7f55d29f889
Esraa102 <manalellban9@gmailcom>
2024-04-01 05:27:21 +0200

    add pre production changes

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
+import path from "path";
 import { connectToDB } from "./config/connectDB.js";
 import { authRouter } from "./routes/auth.route.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { userRouter } from "./routes/user.route.js";
 
 const app = express();
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 connectToDB();
-
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRouter);
